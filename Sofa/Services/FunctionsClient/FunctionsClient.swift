26a138c418bf1ee777a18a393442844185a6d820
dukes32 <luridukes@gmail.com>
2025-11-22 18:05:23 -0500

    debug print result json

--- Sofa/Services/FunctionsClient/FunctionsClient.swift
+++ Sofa/Services/FunctionsClient/FunctionsClient.swift
@@ -35,8 +35,10 @@ final class DefaultFunctionsClient: FunctionsClient {
         let payload = try JSONSerialization.jsonObject(with: request)
         let call = try await functions.httpsCallable(Function.generator).call(payload)
         let data = try JSONSerialization.data(withJSONObject: call.data)
+        #if DEBUG
         let jsonString = String(data: data, encoding: .utf8) ?? ""
         debugPrint(jsonString)
+        #endif
         let response = try decoder.decode(GeneratorResponse.self, from: data)
         return response
     }
